127e5f00747aab6cab799e6184d194efdaf8f636
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2025-02-17 15:30:21 +0300

    парсинг первых 500 аниме в рейтинге

--- main.py
+++ main.py
@@ -5,47 +5,7 @@ from fake_useragent import UserAgent
 import json
 
 #-------------------------------------------------------------------
-# СОХРАНИЛА СТРАНИЦУ, ЧТОБ НЕ ДУДОСИТЬ САЙТ БЕСКОНЕЧНЫМИ ЗАПРОСАМИ
-# ua = UserAgent()
-#
-# headers = {
-#     "Accept": "*/*",
-#     "User-Agent" : ua.random
-# }
-
-# req = requests.get(url="http://www.world-art.ru/animation/rating_top.php",
-#                    headers=headers)
-#
-# src = req.text
-
-# with open("word-art.html", "w", encoding="utf-8") as file:
-#     file.write(src)
-#-------------------------------------------------------------------
-#ИЗ СОХРАНЕННОЙ СТРАНИЦЫ С ПОМОЩЬЮ ПАРСЕРА СОБИРАЮ ССЫЛКИ И НАЗВАНИЯ АНИМЕ С 1 СТРАНИЦЫ РЕЙТИНГА
-# with open("word-art.html", "r", encoding="utf-8") as file:
-#     src = file.read()
-#
-# soup = BeautifulSoup(src, "lxml")
-# hrefs = soup.find_all("a", class_="review")
-#
-#
-# all_anime_dict = {}
-#
-# for item in hrefs:
-#     item_href = item.get("href")
-#
-#     if "votes_history" not in item_href and "limit_" not in item_href:
-        # item_text = item.text
-        # # print(item_text, "___", item_href, end="\n\n")
-        # all_anime_dict[item_text] = item_href
-#
-# with open("anime.json", "w", encoding="utf-8") as file:
-#     json.dump(all_anime_dict, file, indent=4, ensure_ascii=False)
-# ---------------------------------------------------------------------------
-#ТЕПЕРЬ ХОЧУ ПРОЙТИСЬ ПО ПЕРВЫМ 5 СТРАНИЦАМ РЕЙТИНГА И ДОБАВИТЬ АНИМЕ В СПИСОК ФАЙЛА anime.json
-#В СПИСКЕ >2000 АНИМЕПОЭТОМУ ОГРАНИЧУСЬ ПЕРВЫМИ 500 ШТ.
-
-
+# СОХРАНЯЮ СТРАНИЦУ, ЧТОБ НЕ ДУДОСИТЬ САЙТ БЕСКОНЕЧНЫМИ ЗАПРОСАМИ
 ua = UserAgent()
 
 headers = {
@@ -53,6 +13,22 @@ headers = {
     "User-Agent" : ua.random
 }
 
+req = requests.get(url="http://www.world-art.ru/animation/rating_top.php",
+                   headers=headers)
+
+src = req.text
+
+with open("word-art.html", "w", encoding="utf-8") as file:
+    file.write(src)
+
+# ---------------------------------------------------------------------------
+#ИЗ СОХРАНЕННОЙ СТРАНИЦЫ С ПОМОЩЬЮ ПАРСЕРА СОБИРАЮ ССЫЛКИ И НАЗВАНИЯ АНИМЕ.
+#ХОЧУ ПРОЙТИСЬ ПО ПЕРВЫМ 5 СТРАНИЦАМ РЕЙТИНГА И ДОБАВИТЬ АНИМЕ В СПИСОК ФАЙЛА anime.json
+#В СПИСКЕ >2000 АНИМЕ, ПОЭТОМУ ОГРАНИЧУСЬ ПЕРВЫМИ 500 ШТ.
+
+
+# в ссылке на страницу с рейтингом есть параметры limit_1 и limit_2, которые обозначают границы выборки(в данном случае это 100 аниме на 1 страницу).
+# поэтому в цикле будут увеличиваться данные параметры на 100 при каждой итеррации, чтоб проходиться по следующим страницам рейтинга
 limit1 = 0
 limit2 = 100
 
